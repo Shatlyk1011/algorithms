edffec5fa8769943ef6e5e6ad553c202af4d59c3
Abdullayev Shatlyk <gj_wp@mail.ru>
2023-11-14 21:31:43 +0500

    upd

--- 1.linear-search/index.js
+++ 1.linear-search/index.js
@@ -1,5 +1,7 @@
 const names = ["king kong", "ivan", "danya", "sonya", "adam smith", "Antonia Margareti"];
 
+// O(n) - linear
+//best case O(1). worst case O(N); O(1) + O(N) = O(N)
 const linearSearch = (arr, val) => {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === val) return i;
