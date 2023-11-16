9cab22bc81c4ba2b873bb6194ff779ea8c3d89fa
Abdullayev Shatlyk <gj_wp@mail.ru>
2023-11-16 21:13:45 +0500

    string search

--- 3.string-search/index.js
+++ 3.string-search/index.js
@@ -2,9 +2,11 @@ const stringSearch = (string, pattern) => {
   let matches = 0;
   for (let i = 0; i < string.length; i++) {
     for (let j = 0; j < pattern.length; j++) {
-      console.log(string[i], pattern[j]);
+      if (pattern[j] !== string[i + j]) break;
+      if (j === pattern.length - 1) matches++;
     }
   }
+  return matches;
 };
 
-console.log(stringSearch("helloWorld", "hrh"));
+console.log(stringSearch("muchacho", "ch"));
