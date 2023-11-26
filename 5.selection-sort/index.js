b9079f56009182d439c5a4edeb766655a5f4668d
Abdullayev Shatlyk <gj_wp@mail.ru>
2023-11-26 15:40:41 +0500

    add selection/insertion sorts + playground folder

--- 5.selection-sort/index.js
+++ 5.selection-sort/index.js
@@ -2,7 +2,9 @@
 //use case: when you know your data is nearly sorted
 
 const selectionSort = (arr) => {
-  // const swap = (arr, idx1, idx2) => (arr[idx1], arr[idx2] = [arr[idx2], arr[idx1]]); ?? phind
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
   for (let i = 0; i < arr.length; i++) {
     let lowest = i;
 
---
+++ PLAYGROUND/topics.js
@@ -0,0 +1,9 @@
+// MAPS
+
+const map = new Map();
+
+map.set("key", { he: "heheh" });
+
+console.log("map", map);
+
+console.log("GET", map.get("key"));
