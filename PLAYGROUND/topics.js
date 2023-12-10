41f917014d5b3bfdf953b3be70f62db6a1bdb055
Abdullayev Shatlyk <gj_wp@mail.ru>
2023-12-10 17:42:12 +0500

    quick sort implementation

--- PLAYGROUND/topics.js
+++ PLAYGROUND/topics.js
@@ -7,3 +7,13 @@ map.set("key", { he: "heheh" });
 console.log("map", map);
 
 console.log("GET", map.get("key"));
+
+
+//recursive 
+function recursive(num) {
+  if (num === 1) return num;
+
+  return num + recursive(num - 1);
+}
+
+console.log("recursive", recursive(5));
