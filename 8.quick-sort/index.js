665b6ac2a9e0b5d7c870528b0ce7475415fced2f
Abdullayev Shatlyk <gj_wp@mail.ru>
2023-12-10 19:30:02 +0500

    fix time/space complexity of quick sort

--- 8.quick-sort/index.js
+++ 8.quick-sort/index.js
@@ -1,5 +1,5 @@
-//Time complexity - N log^n
-//Space complexity: N
+//Time complexity - BEST CASE - N log^n; AVERAGE CASE - N log^n; WORST CASE - n^2
+//Space complexity: log^n
 function pivot(arr, start = 0, end = arr.length + 1) {
   const swap = (arr, idx1, idx2) => {
     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
