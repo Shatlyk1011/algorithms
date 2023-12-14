cb34ac5a38f5f39e7aea099dced94d4d845d5a48
Abdullayev Shatlyk <gj_wp@mail.ru>
2023-12-14 21:33:30 +0500

    linkedList starter pack

---
+++ DATA_STRUCTURES/SingleLinked/index.js
@@ -0,0 +1,38 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+//understand how it works
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
+}
+
+// let first = new Node("hello");
+// first.next = new Node("there");
+// first.next.next = new Node("how");
+// first.next.next.next = new Node("are");
+// first.next.next.next.next = new Node("you");
+const list = new SinglyLinkedList();
+list.push("HELLO WORLD");
+list.push("SALAM YURT");
--- PLAYGROUND/leetcode.js
+++ PLAYGROUND/leetcode.js
@@ -1,4 +1,4 @@
-// 1) MERGE ARRAYS
+// MERGE ARRAYS
 // Merge nums1 and nums2 into a single array sorted in non-decreasing order.
 //
 // The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
@@ -18,7 +18,6 @@
 // Explanation: The arrays we are merging are [1] and [].
 // The result of the merge is [1].
 
-
 var merge = function (nums1, m, nums2, n) {};
 
 let nums1 = [1, 2, 3, 0, 0, 0];
@@ -26,10 +25,9 @@ let m = 3;
 let nums2 = [2, 5, 6];
 let n = 3;
 
-console.log(merge(nums1, m, nums2, n));
+// console.log(merge(nums1, m, nums2, n));
 
 //EXPECTED [1,2,2,3,5,6]
-
 
 // 2) REMOVE ELEMENTS
 // Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
@@ -46,13 +44,13 @@ console.log(merge(nums1, m, nums2, n));
 // It does not matter what you leave beyond the returned k (hence they are underscores).
 
 // let removeElement = function(nums, val) {
-  // for(let i = nums.length - 1; i >= 0; i--) {
-    // if(nums[i] === val) {
-      // nums.splice( , 1);
-    // }
-  // }
-  // console.log('nums', nums)
-  // return nums;
+// for(let i = nums.length - 1; i >= 0; i--) {
+// if(nums[i] === val) {
+// nums.splice( , 1);
+// }
+// }
+// console.log('nums', nums)
+// return nums;
 // };
 
 // var removeElement = function(nums, val) {
@@ -69,20 +67,19 @@ console.log(merge(nums1, m, nums2, n));
 
 // removeElement([3,2,2,3], 3)
 
-
 // 3) REMOVE ELEMENTS
 // Given an array nums of size n, return the majority element.
-// 
+//
 // The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
-// 
+//
 // Example 1:
-// 
+//
 // Input: nums = [3,2,3]
 // Output: 3
 
 // var majorityElement = function(nums) {
 //   let obj = {}
-  
+
 //   for(let i = 0; i< nums.length; i++){
 //     obj[nums[i]] ? obj[nums[i]] = obj[nums[i]] + 1 : obj[nums[i]] = 1
 //   };
@@ -93,11 +90,9 @@ console.log(merge(nums1, m, nums2, n));
 //   return maxKey
 // }
 
-
 // console.log(majorityElement([1,2,3,4,4,4,5,2,1,2,1,1,1,1,1,1,9,9,9,9,9,9]))
 
-
-// Given an integer x, return true if x is a 
+// Given an integer x, return true if x is a
 // palindrome
 // , and false otherwise.
 
@@ -109,7 +104,6 @@ console.log(merge(nums1, m, nums2, n));
 // const isPalindrome = (num) => {
 //   const toStr = num + "";
 //   const reversed = toStr.split("").reverse().join("");
-
 
 //   return toStr === reversed;
 // };
@@ -129,7 +123,6 @@ console.log(merge(nums1, m, nums2, n));
 
 // console.log("isPalindrom2e", isPalindrome(121));
 
-
 //HOMEWORK
 // Roman to Integer
 // Input: s = "III"
@@ -142,13 +135,9 @@ console.log(merge(nums1, m, nums2, n));
 //   }
 // };
 
-
 //HOMEWORK
 // Write a function to find the longest common prefix string amongst an array of strings.
-
 // If there is no common prefix, return an empty string "".
-
- 
 
 // Example 1:
 
@@ -160,6 +149,10 @@ console.log(merge(nums1, m, nums2, n));
 // Output: ""
 // Explanation: There is no common prefix among the input strings.
 
+const commonPrefix = (arr) => {};
+
+console.log(commonPrefix(["ho", "hell", "held", "hmhm"]));
+
 // Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 
 // An input string is valid if:
@@ -167,7 +160,6 @@ console.log(merge(nums1, m, nums2, n));
 // Open brackets must be closed by the same type of brackets.
 // Open brackets must be closed in the correct order.
 // Every close bracket has a corresponding open bracket of the same type.
- 
 
 // Example 1:
 
