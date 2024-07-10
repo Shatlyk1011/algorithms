158bc99c32f591353ff4df80a189d5b88db3686d
Abdullayev Shatlyk <gj_wp@mail.ru>
2024-07-10 16:22:47 +0500

    repeat: binary search

--- ALGORITHMS/2.binary-search/index.js
+++ ALGORITHMS/2.binary-search/index.js
@@ -19,4 +19,22 @@ const binarySearch = (arr, val) => {
   return arr[middle] === val ? middle : -1;
 };
 
-console.log(binarySearch(data, 200));
+// console.log(binarySearch(data, 200));
+
+const bin2 = (arr, val) => {
+  let start = 0;
+  let end = arr.length - 1;
+  let middle = Math.floor((start + end) / 2);
+
+  //!!! <
+  while (start < end && arr[middle] !== val) {
+    if (arr[middle] < val) start = middle + 1;
+    else end = middle - 1;
+
+    middle = Math.floor((start + end) / 2);
+  }
+
+  return arr[middle] === val ? middle : -1;
+};
+
+console.log(bin2([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 14, 16, 18], 4));
