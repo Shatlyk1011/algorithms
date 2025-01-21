7ed291038bd91d22647bce8dcc1d44484a778ab2
Shatlyk A <gj_wp@mail.ru>
2025-01-21 22:38:07 +0500

    sol: top k frequency

--- SOLUTIONS/top-k-frequent-el.js
+++ SOLUTIONS/top-k-frequent-el.js
@@ -1,9 +1,32 @@
-//bucket sort required
-// https://neetcode.io/problems/top-k-elements-in-list
-const topK = (nums, k) => {
-  let hash = {};
+// my BEST O N * logN
+var topKFrequent = function (nums, k) {
+  let obj = {};
+  let res = [];
+  let final = [];
 
-  let freq = [[]];
+  for (let num of nums) {
+    obj[num] = obj[num] + 1 || 1;
+  }
+
+  for (let key in obj) {
+    let val = obj[key];
+
+    if (res[val]) {
+      res[val].push(+key);
+    } else {
+      res[val] = [+key];
+    }
+  }
+
+  res = res.filter(Boolean);
+
+  for (let i = res.length - 1; i >= 0; i--) {
+    console.log("res[i]");
+    final.push(...res[i]);
+    if (final.length >= k) {
+      return final;
+    }
+  }
 };
 
-console.log(topK([-1, -1], 1));
+console.log(topKFrequent([5, 3, 1, 1, 1, 3, 73, 1], 2));
